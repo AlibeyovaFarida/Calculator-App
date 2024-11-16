510f4032c4634f007f425cf337964c3f5c47e68e
Farida <elibeyovaferide25@gmail.com>
2024-11-16 13:59:33 +0400

    Initial Commit

---
+++ Calculator app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator app
+//
+//  Created by Apple on 16.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
